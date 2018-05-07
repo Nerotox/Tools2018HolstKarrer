91eb40187350e5064f1366b8add04f1bc3a5ea4f
joachim <joachim.holst@gmx.de>
2018-05-07 12:01:27 +0200

    bug fixed

--- JoachimHolst_Nerotox_android/testproj/src/testproj/test.java
+++ JoachimHolst_Nerotox_android/testproj/src/testproj/test.java
@@ -6,11 +6,9 @@ public class test {
 		int kurz = 1;
 	}
 
-
-<<<<<<< HEAD
+	
 	public void thisIsANewMethod() { 
 		System.out.println("gday my dude");
 		String alex = "hunrig";
->>>>>>> e7f5bde8f793148954f01858df0873270ba88e42
 	}
 }
